0d19a2bb99ca7fbe901aca45de48b6e8b8177148
skhjhj <agmlwo@naver.com>
2019-05-23 09:20:41 +0900

    afasf

--- app/src/main/java/com/example/allpet_ver1/profile.java
+++ app/src/main/java/com/example/allpet_ver1/profile.java
@@ -3,15 +3,22 @@ package com.example.allpet_ver1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+
 public class profile extends AppCompatActivity {
     Button register,apply,missions;
-
+    ArrayList<puppy> p;
+    String id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        p = intent.getParcelableArrayListExtra("puppy");
+        id =intent.getExtras().getString("Id");
         setContentView(R.layout.activity_profile);
         register=(Button) findViewById(R.id.register);
         apply=(Button) findViewById(R.id.apply);
@@ -21,19 +28,20 @@ public class profile extends AppCompatActivity {
 
     public void ReOnClick(View view){
         Intent intent = new Intent(this,mypage.class);
-        intent.putExtra("situation",1);
+        intent.putParcelableArrayListExtra("puppy",p);
+        intent.putExtra("Id", id);
         startActivity(intent);
     }
 
     public void ApOnClick(View view){
-        Intent intent = new Intent(this,mypage.class);
+        /*Intent intent = new Intent(this,applying.class);
         intent.putExtra("situation",2);
-        startActivity(intent);
+        startActivity(intent);*/
     }
 
     public void MiOnClick(View view){
-        Intent intent = new Intent(this,mypage.class);
+        /*Intent intent = new Intent(this,mission.class);
         intent.putExtra("situation",3);
-        startActivity(intent);
+        startActivity(intent);*/
     }
 }
